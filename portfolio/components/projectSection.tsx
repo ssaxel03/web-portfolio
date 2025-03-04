ddc00c6329510faf13ebc025a53cf6c39beefb58
axel <ssaxel03@gmail.com>
2025-03-04 22:32:46 +0000

    fix: project button link

--- portfolio/components/projectSection.tsx
+++ portfolio/components/projectSection.tsx
@@ -6,18 +6,15 @@ import { motion } from "framer-motion";
 export default function Projects() {
     return (
         <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true, amount: 0.1 }} transition={{ duration: 0.3 }} >
-            <motion.h2 initial={{ opacity: 0, x: -16 }} whileInView={{ opacity: 1,  x: 0 }} viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.2 }} className='text-xl font-bold mb-4'>Projects</motion.h2>
+            <motion.h2 initial={{ opacity: 0, x: -16 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.2 }} className='text-xl font-bold mb-4'>Projects</motion.h2>
 
             <div id='projects-grid' className='w-full gap-5 items-center justify-center'>
 
                 {projects.list.map((project, index) => (
                     <motion.div key={index} initial={{ opacity: 0, x: -16 }} whileInView={{ opacity: 1, x: 0 }} viewport={{ once: true, amount: 0.15 }} transition={{ duration: 0.5, delay: index * 0.2 }}
                         className='w-full h-full'>
-                        <div className='w-full h-full relative bg-neutral-800 hover:bg-orange-600 hover:scale-105 ease-in-out duration-300 rounded-md pt-2 pb-12 px-2'>
-
-
-                            <Link
-                                href={'/project?id=' + project.id}>
+                        <Link href={'/project?id=' + project.id}>
+                            <div className='w-full h-full relative bg-neutral-800 hover:bg-orange-600 hover:scale-105 ease-in-out duration-300 rounded-md pt-2 pb-12 px-2'>
                                 <h3 className='text-xl mb-1'>{project.name}</h3>
                                 <p className='text-xl mb-1 text-justify'>{project.shortDescription}</p>
 
@@ -36,8 +33,8 @@ export default function Projects() {
                                     ))}
 
                                 </div>
-                            </Link>
-                        </div>
+                            </div>
+                        </Link>
                     </motion.div>
 
                 ))}
